5837bb336c43d89194add5193a000cc4dbdaae44
StephanyeCunto <stephanyecristine6@gmail.com>
2025-05-17 16:04:37 -0300

    Código funcionando

--- RPC/src/client.js
+++ RPC/src/client.js
@@ -4,7 +4,8 @@ const { createClient } = pkg;
 
 const client = createClient({ host: '127.0.0.1', port: 9090, path: '/' });
 
-let name = "agua";
+let name = "teste a";
+
 
 client.methodCall('create', [name], function (error, value) {
   if (error) console.error(error);
@@ -16,3 +17,15 @@ client.methodCall('read',[],function(error,value){
   else console.log('Resposta do servidor:', value);
 });
 
+
+client.methodCall('update',["teste b", 12,5], function(error,value){
+    if (error) console.error(error);
+    else console.log('Resposta do servidor:', value);
+});
+
+let nameDelete = "";
+
+client.methodCall('delete',[nameDelete],function(error,value){
+    if (error) console.error(error);
+    else console.log('Resposta do servidor:', value);
+});
--- RPC/src/serve.js
+++ RPC/src/serve.js
@@ -10,12 +10,27 @@ export class serve{
         this.server = createServer({ host: '0.0.0.0', port: 9090 });
         this.server.on('create', this.handleCreate.bind(this));
         this.server.on('read', this.handleRead.bind(this));
+        this.server.on('update', this.handleUpdate.bind(this));
+        this.server.on('delete', this.handleDelete.bind(this));
+    }
+
+    handleDelete(err, params, callback){
+        const item = this.searchItensName(params[0]);
+        if(item) this.delete(item);
+        const itens = this.getItens();
+        callback(null,itens);
+    }
+
+    handleUpdate(err, params, callback){
+        const item = this.searchItensName(params[0]);
+        if(item) this.update(item,params[1]);
+        const itens = this.getItens();
+        callback(null,itens);
     }
 
     handleRead(err, params, callback){
-        const itens = this.read().Itens;
-        const nameItens = itens.map(itens => itens.Name);
-        callback(null,nameItens);
+        const itens = this.getItensName();
+        callback(null,itens);
     }
 
     handleCreate(err,params, callback){
@@ -23,22 +38,49 @@ export class serve{
         callback(null, `Item adicionado com sucesso.`);
     }
 
-    read(){
+    update(item,price){
+        const itens = this.getItens().filter(itens => itens.Name !== item.Name); 
+        itens.push(this.createItemPurchased(item, price));
+        this.saveItens(itens);
+    }
+
+    delete(item){
+        const itens = this.getItens().filter(itens => itens.Name !== item.Name);
+        this.saveItens(itens);
+    }
+
+    searchItensName(name){
+        const itens = this.getItens();
+        for(let item of itens){
+            if(name == item.Name) return item
+        }
+    }
+
+    getItensName(){
+        return this.getItens().map(itens => itens.Name);
+    }
+
+    getItens(){
         return JSON.parse(fs.readFileSync(localJsonPath, 'utf8'));
     }
 
     create(params){
-        const item = this.createItem(params[0]);
-        const listItens = this.read();
-        listItens.Itens.push(item);
-        fs.writeFileSync(localJsonPath , JSON.stringify(listItens, null, 2));
+        const listItens = this.getItens();
+        listItens.push(this.createItem(params[0]));
+        this.saveItens(listItens);
     }
 
     createItem(item) {
-        const itemObjeto = {Name: item};
-        return itemObjeto;
+        return {Name: item};
     }
 
+    createItemPurchased(item,price){
+        return {Name: item.Name , Preco: price}
+    }
+
+    saveItens(itens){
+        fs.writeFileSync(localJsonPath , JSON.stringify(itens, null, 2));
+    }
 }
 
 const s = new serve();
